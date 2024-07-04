b10f2fc38a59b9e564995f1a6d8eba8a4215f535
Baborikoks <baborikoks@gmail.com>
2024-07-04 22:06:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include "employeemanager.h"
 
 #include <QApplication>
-
+ // Баборико Карина 6 группа
+ // Задание 3-4 по учебной практике 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
